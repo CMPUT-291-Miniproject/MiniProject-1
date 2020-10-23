0522d5cba5986a4f7f2fcb333fe9ec7659fe7c93
Chase Warwick <warwick@cs.ualberta.ca>
2020-10-22 23:32:31 -0600

    Modified WelcomeScreen and main

--- WelcomeScreen.py
+++ WelcomeScreen.py
@@ -1,22 +1,25 @@
 from os import system, name
+import shutil
+
 class WelcomeScreen:
 	def __init__(self):
-		system('cls')
+		system('clear')
 
 	def welcomeUser(self):
-		print("--Welcome User---")
+		screenSize = shutil.get_terminal_size().columns
+		print("-----Welcome User-----".center(screenSize))
+		print("(Type exit to quit at any time)\n\n\n".center(screenSize))
 		
 	def checkUserExistence(self):
 		while True:
-			print("Are you an existing user? (Type y or n)")
-			userInput = input().upper();
+			userInput = input("Are you an existing user? (Type y or n): ").upper()
 			if userInput == "Y":
 				return True
 			elif userInput == "N":
 				return False
+			elif userInput == "EXIT":
+				return None
 			else:
-				print("Invalid Input, please type either y or n")
-				print("Press Enter key to continue")
-				input()
-				system('cls')
+				input("Invalid input, press enter to continue: ")
+				system('clear')
 
